dc8c2937680306fcdab1643a3e467ac708372bee
Seulg2027 <seulgi2027@dgu.ac.kr>
2021-09-25 00:33:20 +0900

    write

--- buckets/urls.py
+++ buckets/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = "buckets"
 urlpatterns = [
   path('bucketList/', view_bucket, name="view_bucket"), # 버킷리스트 카테고리 목록
+  path('write/', write, name="write"), 
 ]
--- buckets/views.py
+++ buckets/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # bucketList
 def view_bucket(request):
   return render(request, 'buckets/bucketlist.html')
+
+def write(request):
+  return render(request, 'buckets/write_bucket.html')
